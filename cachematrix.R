ac9a83fe27942df087b7c8042c33cae7d732534d
Susan Paterson <kcs@kcs-laptop.(none)>
2014-04-26 19:14:59 -0700

    Homework assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Part 1
+# Create a special "matrix" object that can cache its inverse
+makeCacheMatrix<-function(x=matrix()){
+        m<-NULL
 
-## Write a short comment describing this function
+        # Set the value of the matrix
+        # "<<-" used because x and m are defined in parent environment  # Parent environment is MakeCacheMatrix
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+                }
 
-makeCacheMatrix <- function(x = matrix()) {
+        # Get the value of the matrix   
+        get<-function()x
 
+        # Set the value of the inverse matrix
+        # "<<-" used because m is defined in parent environment         # Parent environment is MakeCacheMatrix
+        setinverse<-function(solve) m <<- solve
+
+        # Get the value of the inverse matrix
+        getinverse<-function()m
+
+        # Return a list of the defined functions
+        list(set=set, get=get,
+        setinverse=setinverse,
+        getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Part 2
+# Function to compute the inverse of the special "matrix"
+cacheSolve<-function(x,...){
+
+        # Query the x matrix's cache    
+        m<-x$getinverse()
+
+        # If there is a cache, return the cache's inverse matrix
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+                }
+
+        # If there is not a cache, compute the inverse of the matrix    given in Part 1
+        data<-x$get()
+        m<-solve(data,...)
+        x$setinverse(m)
+
+        # Return the inverse of the matrix      
+        m
 }
